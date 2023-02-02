dec76083bea62f66c8be5a495f18ad622bd29a4b
Abdosh022 <86729100+Abdosh022@users.noreply.github.com>
2023-02-02 02:27:34 +0200

    online-shop

---
+++ admin/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Amiri&family=Cairo:wght@200&family=Poppins:wght@100;200;300&family=Tajawal:wght@300&display=swap" rel="stylesheet">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>online Shop | Add product</title>
+    <link rel="stylesheet" href="index.css">
+</head>
+<body>
+    <center>
+        <div class="main">
+            <form action="insert.php" method="post" enctype="multipart/form-data">
+                <h2>Online shopping site </h2>
+                <img src="online-marketing.jpg" alt="logo" width="300px">
+                <input type="text" name='name'>
+                <br>
+                <input type="text" name='price'>
+                <br>
+                <input type="file" id="file" name='image' style='display:none;'>
+                <label for="file">Choice product image</label>
+                <button name='upload'>upload product ✅</button>
+                <br><br>
+                <a href="products.php"> All avilable products </a>
+                <br>
+                 <a href=../../online-shop/index.php.>buy product</a>
+
+            </form>
+        </div>
+       
+    </center>
+</body>
+</html>
